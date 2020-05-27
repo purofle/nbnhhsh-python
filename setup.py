01f09770e7b209aa36cc33982adcca9dc65abce4
purofle <3272912942@qq.com>
2020-05-27 21:39:46 +0800

    commit

--- setup.py
+++ setup.py
@@ -1,6 +1,22 @@
-from distutils.core import setup
-setup(
-    name="nbnhhsh",
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="nbnhhsh-python",
+    version="0.0.1",
+    author="purofle",
+    author_email="3272912942@qq.com",
     description="nbnhhsh的python版本",
-    version='1.0'
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/purofle/nbnhhsh-python",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache-2.0",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.4',
 )
