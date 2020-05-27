40cdfef43056487d845d153d3128de59d28eb5ce
purofle <3272912942@qq.com>
2020-05-28 00:28:57 +0800

    更新版本

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="nbnhhsh",
-    version="1.0",
+    version="1.1.1",
     author="purofle",
     author_email="3272912942@qq.com",
     description="nbnhhsh的python版本",
